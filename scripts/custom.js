9964c6ba895f4e0bbcbf533099771ff980e71d57
Alex D <spiritxiii@yandex.ru>
2025-05-29 21:37:05 +0300

    bugfix, part 2

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,15 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  document.querySelectorAll('[data-target]').forEach(function(item) {
-    item.addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('[data-target]').forEach(function (item) {
+    item.addEventListener('click', function () {
       document.querySelector('#' + item.dataset.target).showModal();
     });
   });
 
-  document.querySelectorAll('.close-button').forEach(function(item) {
-    item.addEventListener('click', function() {
+  document.querySelectorAll('.close-button').forEach(function (item) {
+    item.addEventListener('click', function () {
       document.querySelector('.dialog[open]').close();
     });
   });
-
 });
